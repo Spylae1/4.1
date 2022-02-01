e49d52f881b729d30c01081a9145713eedfb4f8e
KS <KS@KS-PC>
2022-02-01 10:52:35 +0500

    Добавьте файлы проекта.

---
+++ 4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K;
+            Console.WriteLine("Ввести число ");
+            K = Convert.ToInt32(Console.ReadLine());
+            int[] mas1 = { 1, 3, 4, 8, 6, 9 };
+            Console.WriteLine("Полученный массив: ");
+            for (int i = 0; i < K; i++)
+            {
+
+                Console.WriteLine(mas1[i]);
+            }
+
+            if (mas1[^1] > K)
+            {
+                Console.WriteLine("Результат: " + (mas1[^1]));
+            }
+            else
+                Console.WriteLine(" Результат 0");
+        }
+    }
+}
